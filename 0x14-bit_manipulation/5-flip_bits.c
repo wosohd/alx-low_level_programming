e96c8d866c9a8fe3f1944b203e739e53c1555eb2
wosohd <wosohd@gmail.com>
2023-04-09 01:11:24 -0700

    -mflip_bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,7 +14,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int present;
 	unsigned long int new = n ^ m;
 
-	for (i = 45; i >= 0; i--)
+	for (i = 40; i >= 0; i--)
 	{
 		present = new >> i;
 		if (present & 1)
