f4cec5bddc4619bc79f9f0d32c627bd6eeaec721
wosohd <wosohd@gmail.com>
2023-04-10 13:26:29 -0700

    -mflip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,11 +2,10 @@
 
 /**
  * flip_bits - returns the number of bits to flip
- * to get from one number to another
- * @n: first number
- * @m: second number
+ * @n: first num
+ * @m: second num
  *
- * Return: number of bits to flip
+ * Return: num of bits to flip
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
@@ -14,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int present;
 	unsigned long int new = n ^ m;
 
-	for (i = 40; i >= 0; i--)
+	for (i = 63; i >= 0; i--)
 	{
 		present = new >> i;
 		if (present & 1)
